d849a3de6e2d3052708a46cd480bc3754faf70a7
Guillermo Rivas <guillermo.campanudo@hotmail.com>
2021-10-07 10:47:16 +0100

    Reformatting and commeting some code in the diamondCut

--- contracts/facets/DiamondCutFacet.sol
+++ contracts/facets/DiamondCutFacet.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/******************************************************************************\
-* Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
-* EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
-/******************************************************************************/
-
 import {IDiamondCut} from '../interfaces/IDiamondCut.sol';
 import {LibDiamond} from '../libraries/LibDiamond.sol';
 import {LibWhiteList} from '../libraries/LibWhiteList.sol';
@@ -13,12 +8,12 @@ import 'hardhat/console.sol';
 import './WhiteListFacet.sol';
 
 contract DiamondCutFacet is IDiamondCut {
-    /// @notice Add/replace/remove any number of functions and optionally execute
-    ///         a function with delegatecall
-    /// @param _diamondCut Contains the facet addresses and function selectors
-    /// @param _init The address of the contract or facet to execute _calldata
-    /// @param _calldata A function call, including function selector and arguments
-    ///                  _calldata is executed with delegatecall on _init
+    /**
+      @notice Add/replace/remove any number of functions and optionally execute a function with delegatecall.
+      @param _diamondCut Contains the facet addresses and function selectors.
+      @param _init The address of the contract or facet to execute _calldata.
+      @param _calldata A function call, including function selector and arguments _calldata is executed with delegatecall on _init.
+    **/
     function diamondCut(
         FacetCut[] calldata _diamondCut,
         address _init,
@@ -26,6 +21,10 @@ contract DiamondCutFacet is IDiamondCut {
     ) external override {
         if (LibWhiteList.masterDiamond() != address(0)) {
             for (uint256 facetIndex; facetIndex < _diamondCut.length; facetIndex++) {
+                /**
+                    @dev This functionality is for checking into the whitelist of the master
+                    diamond and see if the facet is approved for use.
+                **/
                 require(
                     WhiteListFacet(LibWhiteList.masterDiamond()).facetWhiteListed(_diamondCut[facetIndex].facetAddress),
                     'diamondMasterCut: NOT_WHITE_LISTED'
