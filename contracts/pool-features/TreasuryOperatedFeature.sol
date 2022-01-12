b708505b825ce35a1a5b466428d9415933237a4b
Bouke Stam <bouke@allianceblock.io>
2022-01-12 15:25:13 +0100

    Slither fix

--- contracts/pool-features/TreasuryOperatedFeature.sol
+++ contracts/pool-features/TreasuryOperatedFeature.sol
@@ -6,8 +6,9 @@ import './../SafeERC20Detailed.sol';
 import './../interfaces/IERC20Detailed.sol';
 import './../RewardsPoolBase.sol';
 import './../TreasuryOperated.sol';
+import './../interfaces/ITreasuryOperated.sol';
 
-abstract contract TreasuryOperatedFeature is RewardsPoolBase, TreasuryOperated {
+abstract contract TreasuryOperatedFeature is ITreasuryOperated, RewardsPoolBase, TreasuryOperated {
     using SafeERC20Detailed for IERC20Detailed;
     address public immutable externalRewardToken;
 
@@ -15,12 +16,12 @@ abstract contract TreasuryOperatedFeature is RewardsPoolBase, TreasuryOperated {
         externalRewardToken = _externalRewardToken;
     }
 
-    function withdrawStake(uint256 amount) public virtual override(TreasuryOperated) onlyTreasury {
+    function withdrawStake(uint256 amount) public virtual override(ITreasuryOperated, TreasuryOperated) onlyTreasury {
         stakingToken.safeTransfer(treasury, amount);
         TreasuryOperated.withdrawStake(amount);
     }
 
-    function notifyExternalReward(uint256 reward) external virtual onlyTreasury {
+    function notifyExternalReward(uint256 reward) external virtual override onlyTreasury {
         TreasuryOperated.notifyExternalReward(externalRewardToken, reward);
     }
 }
