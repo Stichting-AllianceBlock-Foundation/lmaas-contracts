28712725c53fb53380e139027640528ca5df55a4
Bouke Stam <bouke@allianceblock.io>
2022-01-21 11:38:00 +0100

    Remove not old contracts

--- contracts/interfaces/ITreasuryOperated.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
-
-interface ITreasuryOperated {
-    function withdrawStake(uint256 amount) external;
-
-    function notifyExternalReward(uint256 reward) external;
-}
