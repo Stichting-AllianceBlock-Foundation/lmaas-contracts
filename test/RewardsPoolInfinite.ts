02874f75fce77942331638ed764c213aad4f58ce
Nick Vanden Eynde <nick@vitanick.dev>
2023-01-11 16:08:24 +0100

    another push

--- test/RewardsPoolInfinite.ts
+++ test/RewardsPoolInfinite.ts
@@ -342,8 +342,8 @@ describe('RewardsPoolBaseInfinite', () => {
       expect(await rewards[0].balanceOf(stakers[0].address)).to.be.eq(0);
     });
 
-    it.only('Test excess rewards withdrawal bug', async () => {
-      let amount = ethers.utils.parseEther('10');
+    it('Test excess rewards withdrawal bug', async () => {
+      let amount = ethers.utils.parseEther('10000');
       await rewardToken.faucet(rewardsPoolBaseInfinite.address, amount);
 
       await rewardsPoolBaseInfinite['start(uint256)'](3600 * 24 * 5);
@@ -351,14 +351,20 @@ describe('RewardsPoolBaseInfinite', () => {
       amount = ethers.utils.parseEther('10000');
       const staker = stakers[0];
       const staker2 = stakers[1];
+      const staker3 = stakers[2];
+      const staker4 = stakers[3];
 
       stakingToken.faucet(staker.address, amount);
       await stakingToken.connect(staker).approve(rewardsPoolBaseInfinite.address, amount);
-
       await rewardsPoolBaseInfinite.connect(staker).stake(amount);
-      expect(await stakingToken.balanceOf(staker.address)).to.be.eq(0);
-      expect(await stakingToken.balanceOf(rewardsPoolBaseInfinite.address)).to.be.eq(amount);
-      expect(await rewardsPoolBaseInfinite.balanceOf(staker.address)).to.be.eq(amount);
+
+      stakingToken.faucet(staker4.address, amount);
+      await stakingToken.connect(staker4).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker4).stake(amount);
+
+      //   expect(await stakingToken.balanceOf(staker.address)).to.be.eq(0);
+      //   expect(await stakingToken.balanceOf(rewardsPoolBaseInfinite.address)).to.be.eq(amount);
+      //   expect(await rewardsPoolBaseInfinite.balanceOf(staker.address)).to.be.eq(amount);
 
       await timeTravel(3600 * 24 * 2);
       //  day 2 of 5 he deposits reward tokens for the next epoch
@@ -371,6 +377,10 @@ describe('RewardsPoolBaseInfinite', () => {
       // day 12 yet another epoch #3
       await timeTravel(3600 * 24 * 5);
       await rewardToken.faucet(rewardsPoolBaseInfinite.address, amount);
+
+      stakingToken.faucet(staker3.address, amount);
+      await stakingToken.connect(staker3).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker3).stake(amount);
 
       // day 17 yet another epoch #4
       await timeTravel(3600 * 24 * 5);
@@ -388,11 +398,107 @@ describe('RewardsPoolBaseInfinite', () => {
       console.log('staker before', ethers.utils.formatEther(await rewardToken.balanceOf(staker.address)));
       console.log('staker2 before', ethers.utils.formatEther(await rewardToken.balanceOf(staker2.address)));
 
-      await rewardsPoolBaseInfinite.connect(staker).exit();
+      //   await rewardsPoolBaseInfinite.connect(staker).exit();
       await rewardsPoolBaseInfinite.connect(staker2).exit();
+      await rewardsPoolBaseInfinite.connect(staker3).exit();
 
       console.log('staker after', ethers.utils.formatEther(await rewardToken.balanceOf(staker.address)));
       console.log('staker2 after', ethers.utils.formatEther(await rewardToken.balanceOf(staker2.address)));
+      //   console.log(await rewardsPoolBaseInfinite.startTimestamp());
+      //   console.log(await rewardsPoolBaseInfinite.endTimestamp());
+      //   console.log(Math.floor(Date.now() / 1000), Math.floor(Date.now() / 1000 + 3600 * 24 * 5));
+
+      //   console.log(await rewardToken.balanceOf(staker2.address));
+      //   //   await rewardsPoolBaseInfinite.withdrawExcessRewards(signers[0].address);
+      //   console.log(await rewardToken.balanceOf(staker2.address));
+    });
+
+    it.only('Test excess rewards withdrawal bug', async () => {
+      let amount = ethers.utils.parseEther('10000');
+      await rewardToken.faucet(rewardsPoolBaseInfinite.address, amount);
+
+      await rewardsPoolBaseInfinite['start(uint256)'](3600 * 24 * 5);
+
+      amount = ethers.utils.parseEther('10000');
+      const staker = stakers[0];
+      const staker2 = stakers[1];
+      const staker3 = stakers[2];
+      const staker4 = stakers[3];
+
+      stakingToken.faucet(staker.address, amount);
+      await stakingToken.connect(staker).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker).stake(amount);
+
+      stakingToken.faucet(staker4.address, amount);
+      await stakingToken.connect(staker4).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker4).stake(amount);
+
+      //   expect(await stakingToken.balanceOf(staker.address)).to.be.eq(0);
+      //   expect(await stakingToken.balanceOf(rewardsPoolBaseInfinite.address)).to.be.eq(amount);
+      //   expect(await rewardsPoolBaseInfinite.balanceOf(staker.address)).to.be.eq(amount);
+
+      await timeTravel(3600 * 24 * 2);
+      //  day 2 of 5 he deposits reward tokens for the next epoch
+      await rewardToken.faucet(rewardsPoolBaseInfinite.address, amount);
+
+      stakingToken.faucet(staker.address, amount);
+      await stakingToken.connect(staker).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker).stake(amount);
+
+      // day 7 next epoch did start #2
+      await timeTravel(3600 * 24 * 5);
+      await rewardToken.faucet(rewardsPoolBaseInfinite.address, amount);
+
+      stakingToken.faucet(staker4.address, amount);
+      await stakingToken.connect(staker4).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker4).stake(amount);
+
+      // day 12 yet another epoch #3
+      await timeTravel(3600 * 24 * 5);
+      await rewardToken.faucet(rewardsPoolBaseInfinite.address, amount);
+
+      stakingToken.faucet(staker3.address, amount);
+      await stakingToken.connect(staker3).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker3).stake(amount);
+
+      // day 17 yet another epoch #4
+      await timeTravel(3600 * 24 * 5);
+      await rewardToken.faucet(rewardsPoolBaseInfinite.address, amount);
+
+      stakingToken.faucet(staker3.address, amount);
+      await stakingToken.connect(staker3).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker3).stake(amount);
+
+      // day 22 yet another epoch #5
+      await timeTravel(3600 * 24 * 5);
+      await rewardToken.faucet(rewardsPoolBaseInfinite.address, amount);
+
+      stakingToken.faucet(staker3.address, amount);
+      await stakingToken.connect(staker3).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker3).stake(amount);
+
+      stakingToken.faucet(staker2.address, amount);
+      await stakingToken.connect(staker2).approve(rewardsPoolBaseInfinite.address, amount);
+      await rewardsPoolBaseInfinite.connect(staker2).stake(amount);
+
+      await timeTravel(3600 * 24 * 5);
+      console.log('staker before', ethers.utils.formatEther(await rewardToken.balanceOf(staker.address)));
+
+      //   await rewardsPoolBaseInfinite.connect(staker).exit();
+      console.log('staker2 before', ethers.utils.formatEther(await rewardToken.balanceOf(staker2.address)));
+
+      await rewardsPoolBaseInfinite.connect(staker2).exit();
+      console.log('staker2 after', ethers.utils.formatEther(await rewardToken.balanceOf(staker2.address)));
+
+      console.log('staker3 before', ethers.utils.formatEther(await rewardToken.balanceOf(staker3.address)));
+
+      await rewardsPoolBaseInfinite.connect(staker3).exit();
+      console.log('staker3 after', ethers.utils.formatEther(await rewardToken.balanceOf(staker3.address)));
+
+      console.log('staker4 before', ethers.utils.formatEther(await rewardToken.balanceOf(staker4.address)));
+      await rewardsPoolBaseInfinite.connect(staker4).exit();
+      console.log('staker4 after', ethers.utils.formatEther(await rewardToken.balanceOf(staker4.address)));
+
       //   console.log(await rewardsPoolBaseInfinite.startTimestamp());
       //   console.log(await rewardsPoolBaseInfinite.endTimestamp());
       //   console.log(Math.floor(Date.now() / 1000), Math.floor(Date.now() / 1000 + 3600 * 24 * 5));
