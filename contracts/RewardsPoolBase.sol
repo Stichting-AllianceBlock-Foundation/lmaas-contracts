f39e8ede3be8d69e9f57afc018bcb63a674bb489
guillermoriv <guillermo.campanudo@gmail.com>
2023-01-25 10:45:45 +0000

    :mute: Removing console.sol

--- contracts/RewardsPoolBase.sol
+++ contracts/RewardsPoolBase.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.9;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import 'hardhat/console.sol';
 
 /** @dev Base pool contract used in all other pools. 
 Users can stake tokens and get rewards based on the percentage of total staked tokens.
